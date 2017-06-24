67f4da51c8acccb2cdd3e5f2d9e197e04745b6c4
Daniel Borchmann <daniel@algebra20.de>
2017-06-24 20:34:47 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,4 @@
-(defproject conexp-clj-programming "nil"
-  :min-lein-version "1.3.0"
-  :description "Fun with conexp-clj"
-  :dependencies [[conexp-clj/conexp-clj "0.0.7-alpha-SNAPSHOT"]]
-  :keep-non-project-classes true
-  :jvm-opts ["-server", "-Xmx1g"]
-  :global-vars {*warn-on-reflection* true}
-  ;:test-paths ["src/tests/"]
-  )
+(defproject conexp-clj-programming "0.0.0"
+  :min-lein-version "2.0.0"
+  :description "Additional Code for conexp-clj"
+  :dependencies [[conexp-clj/conexp-clj "1.1.7"]])
